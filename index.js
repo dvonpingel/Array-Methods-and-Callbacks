94fa0e9ad16ca0bd5264b4548b867c05e44cf097
D'Artagnan von Pingel <dvonpingel@gmail.com>
2020-12-15 12:34:37 -0700

    first commit, did task 1a

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ import { fifaData } from './fifa.js';
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
 
 //(a) Home Team name for 2014 world cup final
+function homeTeamName(fifa) {
+    return fifa.Year === 2014 && fifa.Stage === "Final";
+};
+let homeTeam = fifaData.filter(homeTeamName);
+console.log(homeTeam[0]['Home Team Name']);
 
 //(b) Away Team name for 2014 world cup final
 
